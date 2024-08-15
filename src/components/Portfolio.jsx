297bf94ac15fcb2ce7b4a7bc740e8a6b0ac0c7aa
Anthony <anthonypenaloza99@gmail.com>
2024-08-15 16:47:02 -0500

    styled the portfolio page

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -18,35 +18,46 @@ const Portfolio = () => {
 
     return (
         <div>
-            <h2>Portfolio</h2>
-            <div>
-                <video controls className="portfolio-videos" >
-                    <source src={ceilingB}/>
-                </video>
-                <video controls className="portfolio-videos" >
-                    <source src={ceilingA}/>
-                </video>
-                <video controls className="portfolio-videos" >
-                    <source src={BathroomB}/>
-                </video>
-                <img src={Stairs} className="portfolio-images"></img>
-                <video controls className="portfolio-videos" >
-                    <source src={BandD}/>
-                </video>
-                <img src={DryWall} className="portfolio-images"></img>
-                <img src={PaintedDryWall} className="portfolio-images"></img>
-                <video controls className="portfolio-videos" >
-                    <source src={house1B}/>
-                </video>
-                <video controls className="portfolio-videos" >
-                    <source src={house1A}/>
-                </video>
-                <video controls className="portfolio-videos" >
-                    <source src={house3A}/>
-                </video>
-                <video controls className="portfolio-videos" >
-                    <source src={house3}/>
-                </video>
+            <p id="description">
+                Our portfolio showcases a diverse range of home painting projects, emphasizing meticulous attention to detail and exceptional craftsmanship. Whether it's vibrant interiors, durable exteriors, or work at high altitudes up to 40 feet, each project reflects our dedication to transforming spaces with precision. Explore our work to see how we bring color and life to every home, no matter the challenge.</p>
+            <div className="portfolio-containers">
+                <div className="portfolio-container">
+                    <video controls className="portfolio-videos" >
+                        <source src={ceilingB} />
+                    </video>
+                    <video controls className="portfolio-videos" >
+                        <source src={ceilingA} />
+                    </video>
+                </div>
+                <div className="portfolio-container">
+                    <video controls className="portfolio-videos" >
+                        <source src={BathroomB} />
+                    </video>
+                    <img src={Stairs} className="portfolio-images"></img>
+                    <video controls className="portfolio-videos" >
+                        <source src={BandD} />
+                    </video>
+                </div>
+                <div className="portfolio-container">
+                    <img src={DryWall} className="portfolio-images"></img>
+                    <img src={PaintedDryWall} className="portfolio-images"></img>
+                </div>
+                <div className="portfolio-container">
+                    <video controls className="portfolio-videos" >
+                        <source src={house1B} />
+                    </video>
+                    <video controls className="portfolio-videos" >
+                        <source src={house1A} />
+                    </video>
+                </div>
+                <div className="portfolio-container">
+                    <video controls className="portfolio-videos" >
+                        <source src={house3} />
+                    </video>
+                    <video controls className="portfolio-videos" >
+                        <source src={house3A} />
+                    </video>
+                </div>
             </div>
         </div>
     )
